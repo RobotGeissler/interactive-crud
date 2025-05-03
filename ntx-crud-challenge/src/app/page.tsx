38436c5978d2ab3b133dd1d710e97b128c385813
Harrison Geissler <Robotgeissler@gmail.com>
2025-05-03 14:46:16 -0400

    Consolidating population logic into function

--- ntx-crud-challenge/src/app/page.tsx
+++ ntx-crud-challenge/src/app/page.tsx
@@ -1,6 +1,23 @@
 import Image from "next/image";
 import { Todo } from "@/lib/types";
 
+// Pick three random indexes from the todos array, shuffle & pick 
+// fisher yates shuffle algorithm is fine since each todo has a unique id
+function PopulateTodos(todos: Todo[], length: number) {
+  const indices = Array.from({ length: todos.length }, (_, i) => i);
+  for (let i = indices.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [indices[i], indices[j]] = [indices[j], indices[i]];
+  }
+  // Double check that the resulting todos have unique ids
+  const uniqueIds = new Set(todos.map((todo) => todo.id));
+  if (uniqueIds.size !== todos.length) {
+    return false;
+  }
+  const todolist = indices.slice(0, 3).map((index) => todos[index]);
+  return todolist;
+}
+
 export default async function MainPage() {
   try {
     const response = await fetch("https://jsonplaceholder.typicode.com/todos");
@@ -8,19 +25,13 @@ export default async function MainPage() {
     if (!response.ok) {
       return <div className="text-red-500">Error fetching data</div>;
     }
-    // Pick three random indexes from the todos array, shuffle & pick 
-    // fisher yates shuffle algorithm is fine since each todo has a unique id
-    const indices = Array.from({ length: todos.length }, (_, i) => i);
-    for (let i = indices.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
-      [indices[i], indices[j]] = [indices[j], indices[i]];
+    if (todos.length < 3) {
+      return <div className="text-red-500">Not enough todos</div>;
     }
-    // Double check that the todos have unique ids
-    const uniqueIds = new Set(todos.map((todo) => todo.id));
-    if (uniqueIds.size !== todos.length) {
-      return <div className="text-red-500">Error: Todos have duplicate ids</div>;
+    const todolist = PopulateTodos(todos, 3);
+    if (!todolist) {
+      return <div className="text-red-500">Error: Todos have duplicate ids</div>
     }
-    const todolist = indices.slice(0, 3).map((index) => todos[index]);
     return (
       <div className="flex flex-col items-center justify-center min-h-screen bg-black-100">
         <h1 >Random Todos</h1>
